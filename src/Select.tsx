13ddde4c4548c94dc13acca16fefe27f24854a69
kiyarashnd <kiarashnadri@yahoo.com>
2023-07-20 17:40:59 +0330

    work on select component and add Select css file

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,6 +1,8 @@
+import styles from './select.module.css'
+
 type selectOptions={
     label:string
-    value:string
+    value:any
 }
 
 type SelectProps ={
@@ -10,5 +12,20 @@ onChange:(value:selectOptions | undefined)=>void
 }
 
 export function Select({value,onChange,options}:SelectProps){
-
+    return (
+        <>
+        <div className={styles.container}>
+            <span className={styles.value}>Value</span>
+            {/* here &times; is X icon */}
+            <button className={styles['clear-btn']}>&times;</button>
+            <div className={styles.divider}></div>
+            <div className={styles.caret}></div>
+            <ul className={styles.options}>
+                {options.map(option=>(
+                    <li key={option.label} className={styles.option}>{option.label}</li>
+                ))}
+            </ul>
+        </div>
+        </>
+    )
 }
