bb727462094a16929108e31736133275e438f88b
kiyarashnd <kiarashnadri@yahoo.com>
2023-07-20 18:50:08 +0330

    finish css styles

--- src/Select.tsx
+++ src/Select.tsx
@@ -14,13 +14,13 @@ onChange:(value:selectOptions | undefined)=>void
 export function Select({value,onChange,options}:SelectProps){
     return (
         <>
-        <div className={styles.container}>
+        <div tabIndex={0} className={styles.container}>
             <span className={styles.value}>Value</span>
             {/* here &times; is X icon */}
             <button className={styles['clear-btn']}>&times;</button>
             <div className={styles.divider}></div>
             <div className={styles.caret}></div>
-            <ul className={styles.options}>
+            <ul className={`${styles.options} ${styles.show}`}>
                 {options.map(option=>(
                     <li key={option.label} className={styles.option}>{option.label}</li>
                 ))}
